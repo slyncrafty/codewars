02eec9350910762fd9e8ff85bdb8d3a297deb595
slyncrafty <101752232+slyncrafty@users.noreply.github.com>
2025-05-14 23:43:05 +0100

    Added Solutions

---
+++ 7kyu/Mumbling.js
@@ -0,0 +1,39 @@
+/* ========== ========== ========== ========== ========== ==========*/
+// https://www.codewars.com/kata/5667e8f4e3f572a8f2000039
+/* ========== ========== ========== ========== ========== ==========*/
+/*
+Mumbling
+
+Description:
+
+This time no story, no theory. The examples below show you how to write function accum:
+Examples:
+
+accum("abcd") -> "A-Bb-Ccc-Dddd"
+accum("RqaEzty") -> "R-Qq-Aaa-Eeee-Zzzzz-Tttttt-Yyyyyyy"
+accum("cwAt") -> "C-Ww-Aaa-Tttt"
+
+The parameter of accum is a string which includes only letters from a..z and A..Z.
+*/
+
+
+
+// Solution
+
+function accum(s) {
+    let accumulated = [];
+    for(let i = 0; i < s.length; i++) {
+        const char = s[i];
+        accumulated.push(char.toUpperCase() + char.toLowerCase().repeat(i));
+    }
+    return accumulated.join('-');
+}
+
+
+
+// Test Codes
+console.log(accum("ZpglnRxqenU") === "Z-Pp-Ggg-Llll-Nnnnn-Rrrrrr-Xxxxxxx-Qqqqqqqq-Eeeeeeeee-Nnnnnnnnnn-Uuuuuuuuuuu");
+console.log(accum("NyffsGeyylB") === "N-Yy-Fff-Ffff-Sssss-Gggggg-Eeeeeee-Yyyyyyyy-Yyyyyyyyy-Llllllllll-Bbbbbbbbbbb");
+console.log(accum("MjtkuBovqrU") === "M-Jj-Ttt-Kkkk-Uuuuu-Bbbbbb-Ooooooo-Vvvvvvvv-Qqqqqqqqq-Rrrrrrrrrr-Uuuuuuuuuuu");
+console.log(accum("EvidjUnokmM") === "E-Vv-Iii-Dddd-Jjjjj-Uuuuuu-Nnnnnnn-Oooooooo-Kkkkkkkkk-Mmmmmmmmmm-Mmmmmmmmmmm");
+console.log(accum("HbideVbxncC") === "H-Bb-Iii-Dddd-Eeeee-Vvvvvv-Bbbbbbb-Xxxxxxxx-Nnnnnnnnn-Cccccccccc-Ccccccccccc");
