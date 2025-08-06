546ad6189a1cfe429e8e0f252690fac40745a4d4
slyncrafty <101752232+slyncrafty@users.noreply.github.com>
2025-08-06 17:19:43 +0900

    Added Solution Product-Array_Array-Series-5.js

---
+++ 7kyu/Product-Array_Array-Series-5.js
@@ -0,0 +1,74 @@
+/* ========== ========== ========== ========== ========== ==========*/
+// https://www.codewars.com/kata/5a905c2157c562994900009d
+/* ========== ========== ========== ========== ========== ==========*/
+/*
+Product Array (Array Series #5)
+
+Description:
+Task
+
+Given an array/list [] of integers , Construct a product array Of same size Such That prod[i] is equal to The Product of all the elements of Arr[] except Arr[i].
+Notes
+
+    Array/list size is at least 2 .
+
+    Array/list's numbers Will be only Positives
+
+    Repetition of numbers in the array/list could occur.
+
+Input >> Output Examples
+
+productArray ({12,20}) ==>  return {20,12}
+
+Explanation:
+
+    The first element in prod [] array 20 is the product of all array's elements except the first element
+
+    The second element 12 is the product of all array's elements except the second element .
+
+productArray ({1,5,2}) ==> return {10,2,5}
+
+Explanation:
+
+    The first element 10 is the product of all array's elements except the first element 1
+
+    The second element 2 is the product of all array's elements except the second element 5
+
+    The Third element 5 is the product of all array's elements except the Third element 2.
+
+productArray ({10,3,5,6,2}) return ==> {180,600,360,300,900}
+
+Explanation:
+
+    The first element 180 is the product of all array's elements except the first element 10
+
+    The second element 600 is the product of all array's elements except the second element 3
+
+    The Third element 360 is the product of all array's elements except the third element 5
+
+    The Fourth element 300 is the product of all array's elements except the fourth element 6
+
+    Finally ,The Fifth element 900 is the product of all array's elements except the fifth element 2
+
+*/
+
+
+
+// Solution
+function productArray(numbers){
+  const totalProduct = numbers.reduce((acc, curr) => acc*curr, 1);
+  const resArray = Array(numbers.length).fill(1);
+  for(let i = 0; i < numbers.length; i++) {
+    if(numbers[i] === 0) resArray[i] = totalProduct;
+    else resArray[i] = totalProduct / numbers[i];  
+  }
+  return resArray;
+}
+
+
+
+// Test Codes
+console.log(productArray([12, 20]), [20, 12]);
+console.log(productArray([3, 27, 4, 2]), [216, 24, 162, 324]);
+console.log(productArray([13, 10, 5, 2, 9]), [900, 1170, 2340, 5850, 1300]);
+console.log(productArray([16, 17, 4, 3, 5, 2]), [2040, 1920, 8160, 10880, 6528, 16320]);
