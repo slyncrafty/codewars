28a7e599ef6e4c64e68b20aab0365e3cb4415b0b
slyncrafty <101752232+slyncrafty@users.noreply.github.com>
2025-11-17 13:15:18 +0000

    Add Solution Crack-the-PIN.js

---
+++ 6kyu/Crack-the-PIN.js
@@ -0,0 +1,44 @@
+/* ========== ========== ========== ========== ========== ==========*/
+// https://www.codewars.com/kata/5a0366f12b651dbfa300000c
+/* ========== ========== ========== ========== ========== ==========*/
+/*
+Crack the PIN
+
+Description:
+Given is a md5 hash of a five digits long PIN. It is given as string. Md5 is a function to hash your password: "password123" ===> "482c811da5d5b4bc6d497ffa98491e38"
+
+Why is this useful? Hash functions like md5 can create a hash from string in a short time and it is impossible to find out the password, if you only got the hash. The only way is cracking it, means try every combination, hash it and compare it with the hash you want to crack. (There are also other ways of attacking md5 but that's another story) Every Website and OS is storing their passwords as hashes, so if a hacker gets access to the database, he can do nothing, as long the password is safe enough.
+
+What is a hash?
+
+What is md5?
+
+Note: Many languages have build in tools to hash md5. If not, you can write your own md5 function or google for an example.
+
+Here is another kata on generating md5 hashes!
+
+Your task is to return the cracked PIN as string.
+
+This is a little fun kata, to show you, how weak PINs are and how important a bruteforce protection is, if you create your own login.
+
+If you liked this kata, here is an extension with short passwords!/
+*/
+
+// Solution
+const crypto = require('crypto');
+function crack(hash) {
+	for (let i = 0; i < 100000; i++) {
+		const pin = String(i).padStart(5, '0');
+		if (crypto.createHash('md5').update(pin).digest('hex') === hash) return pin;
+	}
+	return null;
+}
+
+// Test Codes
+const assertEqual = (a, b) => {
+	if (a === b) console.log('Correct');
+	else console.log('Incorrect', a, b);
+};
+assertEqual(crack('827ccb0eea8a706c4c34a16891f84e7b'), '12345');
+
+assertEqual(crack('86aa400b65433b608a9db30070ec60cd'), '00078');
