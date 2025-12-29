f3126ca2c5dfc7426b28234c94e1f434a42d6632
slyncrafty <101752232+slyncrafty@users.noreply.github.com>
2025-12-29 23:10:24 +0000

    Add Solution One-down.js

---
+++ 6kyu/One-down.js
@@ -0,0 +1,51 @@
+/* ========== ========== ========== ========== ========== ==========*/
+// https://www.codewars.com/kata/56419475931903e9d1000087/
+/* ========== ========== ========== ========== ========== ==========*/
+/*
+One down
+
+Description:
+A very passive-aggressive co-worker of yours was just fired. While he was gathering his things, he quickly inserted a bug into your system which renamed everything to what looks like jibberish. He left two notes on his desk, one reads: "ABCDEFGHIJKLMNOPQRSTUVWXYZabcdefghijklmnopqrstuvwxyz" while the other reads: "Uif usjdl up uijt lbub jt tjnqmf kvtu sfqmbdf fwfsz mfuufs xjui uif mfuufs uibu dpnft cfgpsf ju".
+
+Rather than spending hours trying to find the bug itself, you decide to try and decode it.
+
+In case your language allows it and the input is not a string, your function must return "Input is not a string". Your function must be able to handle capital and lower case letters. You will not need to worry about punctuation.
+
+*/
+
+// Solution
+function oneDown(str) {
+	if (typeof str !== 'string') return 'Input is not a string';
+
+	const upper = 'ABCDEFGHIJKLMNOPQRSTUVWXYZ';
+	const lower = 'abcdefghijklmnopqrstuvwxyz';
+
+	let result = '';
+
+	for (const ch of str) {
+		if (upper.includes(ch)) {
+			const i = upper.indexOf(ch);
+			result += i === 0 ? 'z' : upper[i - 1];
+		} else if (lower.includes(ch)) {
+			const i = lower.indexOf(ch);
+			result += i === 0 ? 'Z' : lower[i - 1];
+		} else {
+			result += ch;
+		}
+	}
+
+	return result;
+}
+
+// Test Codes
+const equal = (actual, expected) => {
+	if (actual === expected) console.log('✅PASSED');
+	else console.log('❌FAILED', actual, 'is expected to match', expected);
+};
+
+equal(oneDown('Ifmmp'), 'Hello');
+equal(
+	oneDown('Uif usjdl up uijt lbub jt tjnqmf'),
+	'The trick to this kata is simple'
+);
+equal(oneDown(45), 'Input is not a string');
