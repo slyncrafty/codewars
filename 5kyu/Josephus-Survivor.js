1f5fbe3dfa24ce37bb4a56bd3ddebb692e400f25
slyncrafty <101752232+slyncrafty@users.noreply.github.com>
2026-01-07 13:25:27 +0000

    Add Solution Josephus-Survivor.js

---
+++ 5kyu/Josephus-Survivor.js
@@ -0,0 +1,49 @@
+/* ========== ========== ========== ========== ========== ==========*/
+// https://www.codewars.com/kata/555624b601231dc7a400017a
+/* ========== ========== ========== ========== ========== ==========*/
+/*
+Josephus Survivor
+
+Description:
+In this kata you have to correctly return who is the "survivor", ie: the last element of a Josephus permutation.
+
+Basically you have to assume that n people are put into a circle and that they are eliminated in steps of k elements, like this:
+
+n=7, k=3 => means 7 people in a circle
+one every 3 is eliminated until one remains
+[1,2,3,4,5,6,7] - initial sequence
+[1,2,4,5,6,7] => 3 is counted out
+[1,2,4,5,7] => 6 is counted out
+[1,4,5,7] => 2 is counted out
+[1,4,5] => 7 is counted out
+[1,4] => 5 is counted out
+[4] => 1 counted out, 4 is the last element - the survivor!
+
+The above link about the "base" kata description will give you a more thorough insight about the origin of this kind of permutation, but basically that's all that there is to know to solve this kata.
+
+Notes and tips: using the solution to the other kata to check your function may be helpful, but as much larger numbers will be used, using an array/list to compute the number of the survivor may be too slow; you may assume that both n and k will always be >=1.
+
+*/
+
+// Solution
+function josephusSurvivor(n, k) {
+	const arr = Array.from({ length: n }, (_, i) => i + 1);
+	let idx = 0;
+	while (arr.length > 1) {
+		idx = (idx + k - 1) % arr.length;
+		arr.splice(idx, 1);
+	}
+	return arr[0];
+}
+
+// Test Codes
+const strictEqual = (a, b) => {
+	if (a === b) console.log('✅PASSED');
+	else console.log('❌FAILED');
+};
+
+strictEqual(josephusSurvivor(7, 3), 4);
+strictEqual(josephusSurvivor(11, 19), 10);
+strictEqual(josephusSurvivor(1, 300), 1);
+strictEqual(josephusSurvivor(14, 2), 13);
+strictEqual(josephusSurvivor(100, 1), 100);
